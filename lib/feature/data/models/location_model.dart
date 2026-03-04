9d136c6ef9bc259658b6764eb0c55bed11f5944e
bakebrlk <bakebrlk@gmail.com>
2026-03-05 01:40:29 +0500

    created location and person model

---
+++ lib/feature/data/models/location_model.dart
@@ -0,0 +1,13 @@
+import 'package:rick_and_morty/feature/domain/entities/location_entity.dart';
+
+class LocationModel extends LocationEntity {
+  LocationModel({required super.name, required super.url});
+
+  factory LocationModel.fromJson(Map<String, dynamic> json) {
+    return LocationModel(name: json['name'], url: json['url']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'name': name, 'url': url};
+  }
+}
---
+++ lib/feature/data/models/person_model.dart
@@ -0,0 +1,52 @@
+import 'package:rick_and_morty/feature/data/models/location_model.dart';
+import 'package:rick_and_morty/feature/domain/entities/person_entity.dart';
+
+class PersonModel extends PersonEntity {
+  const PersonModel({
+    required super.id,
+    required super.name,
+    required super.status,
+    required super.species,
+    required super.type,
+    required super.gender,
+    required super.origin,
+    required super.location,
+    required super.image,
+    required super.episode,
+    required super.created,
+  });
+
+  factory PersonModel.fromJson(Map<String, dynamic> json) {
+    return PersonModel(
+      id: json['id'],
+      name: json['name'],
+      status: json['status'],
+      species: json['species'],
+      type: json['type'],
+      gender: json['gender'],
+      origin: LocationModel.fromJson(json['origin']),
+      location: LocationModel.fromJson(json['location']),
+      image: json['image'],
+      episode: (json['episode'] as List<dynamic>)
+          .map((e) => e as String)
+          .toList(),
+      created: DateTime.parse(json['created'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'status': status,
+      'species': species,
+      'type': type,
+      'gender': gender,
+      'origin': origin,
+      'location': location,
+      'image': image,
+      'episode': episode,
+      'created': created.toIso8601String(),
+    };
+  }
+}
